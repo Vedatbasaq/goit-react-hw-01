c71638024963f3f1f2cd9c6084fbb25c208d1145
VEDAT <vedatbasaq@gmail.com>
2025-08-16 23:57:44 +0300

    react homework update

--- src/components/FriendList/FriendItem.jsx
+++ src/components/FriendList/FriendItem.jsx
@@ -2,7 +2,7 @@ import css from './FriendAll.module.css';
 
 const FriendItem = ({ avatar, name, isOnline }) => {
   return (
-    <div className={css.friendListItem}>
+    <div className={css.friendItem}>
       <img src={avatar} alt="Avatar" width="48" />
       <p className={css.name}>{name}</p>
       <p className={isOnline ? css.online : css.offline}>
